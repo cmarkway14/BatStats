5d4033d95b35c9cb6fd3996df965945631653a5d
Curtis Markway <cjmdr2@mwc-010160.dhcp.missouri.edu>
2016-11-01 20:10:34 -0500

    Initial Commit

---
+++ BatStats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BatStats
+//
+//  Created by Curtis Markway on 11/1/16.
+//  Copyright © 2016 Curtis Markway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
