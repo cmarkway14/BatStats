67512ee160087f73a6da15950b0e439a377a1f02
Curtis Markway <cjmdr2@curtiss-mbp.centurylink.net>
2016-11-06 14:24:51 -0600

    ui creation

---
+++ BatStats/SearchViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.swift
+//  BatStats
+//
+//  Created by Curtis Markway on 11/6/16.
+//  Copyright © 2016 Curtis Markway. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
